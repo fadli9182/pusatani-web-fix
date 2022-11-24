c390674dfeaf71a170868e46efa0f738cc3b8cf8
fadli9182 <rosyidfadli74@gmail.com>
2022-11-25 03:23:18 +0700

    ketinggalan

--- src/components/pages/DaftarPabrik.jsx
+++ src/components/pages/DaftarPabrik.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
